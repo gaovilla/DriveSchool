92f26dfba37e6ada4995d6a73b947fada0fce0ee
zxc <xuecheng.zhang@bitbao.net>
2015-03-10 11:03:17 +0800

    10号和高博合并代码

--- DriveSchool/ViewController/main/DSTeacherDetailViewController.m
+++ DriveSchool/ViewController/main/DSTeacherDetailViewController.m
@@ -8,7 +8,7 @@
 
 #import "DSTeacherDetailViewController.h"
 
-@interface DSTeacherDetailViewController ()
+@interface DSTeacherDetailViewController ()<UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *moneyLabel;
 @property (weak, nonatomic) IBOutlet UILabel *telLabel;
@@ -19,8 +19,10 @@
 @property (strong, nonatomic) NSMutableDictionary *teacherInfo;
 @property (weak, nonatomic) IBOutlet UIView *teacherInfoView;
 @property (weak, nonatomic) IBOutlet UIView *infoVoew;
-
-
+@property (weak, nonatomic) IBOutlet UIScrollView *bannerScrollView;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (strong, nonatomic) NSTimer *bannerTimer;
+@property (strong, nonatomic) NSMutableArray *bannerArray;
 
 
 @end
@@ -31,6 +33,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self setDefaultValue];
+    [self initBanner];
     
 }
 -(void)setDefaultValue{
@@ -40,9 +43,49 @@
     _infoVoew.layer.borderWidth = 1;
     _infoVoew.layer.borderColor = [[UIColor lightGrayColor]CGColor];
     
-    [_scrollView setContentSize:CGSizeMake(CURRENT_WIDTH, 800)];
-    [self initTeacherInfo];
+   
     
+    [self initTeacherInfo];
+    //要计算教练简介的高度来设置
+    //因为要去除不滑动显示的内容高度,所以减去50
+    CGFloat height  = [AppUtil heightForString:[_teacherInfo objectForKey:@"introduce"] fontSize:17.0 andWidth:290]-50;
+    
+    [_scrollView setContentSize:CGSizeMake(CURRENT_WIDTH, CURRENT_HEIGHT+200)];
+}
+-(void)initBanner{
+    _bannerArray = [[NSMutableArray alloc]init];
+    NSDictionary *bannerOne = @{@"image":@"icon_main_banner_one.png",@"url":@"http://baidu.com"};
+    NSDictionary *bannerTwo = @{@"image":@"icon_main_banner_four.jpg",@"url":@"http://baidu.com"};
+    [_bannerArray addObject:bannerOne];
+    [_bannerArray addObject:bannerTwo];
+    [_bannerScrollView setContentSize:CGSizeMake(CURRENT_WIDTH*_bannerArray.count, 160)];
+    
+    for (int i = 0; i < _bannerArray.count; i++) {
+        NSDictionary *dic = [_bannerArray objectAtIndex:i];
+        NSString *imageName = [dic nonNullValueForKey:@"image"];
+        UIButton *bannerBtn = [[UIButton alloc]initWithFrame:CGRectMake(i*CURRENT_WIDTH, 0, CURRENT_WIDTH, 160)];
+        [bannerBtn setBackgroundImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
+        
+        [_bannerScrollView addSubview:bannerBtn];
+    }
+    
+    [_pageControl setNumberOfPages:_bannerArray.count];
+    if(!_bannerTimer){
+        _bannerTimer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(bannerAutoScroll) userInfo:nil repeats:YES];
+    }
+}
+-(void)bannerAutoScroll{
+    NSInteger bannerCount = _pageControl.numberOfPages;
+    NSInteger currentPage = _pageControl.currentPage;
+    if (currentPage < bannerCount-1) {
+        CGPoint point = CGPointMake(CURRENT_WIDTH*currentPage+CURRENT_WIDTH,0);
+        [_bannerScrollView setContentOffset:point animated:YES];
+        [_pageControl setCurrentPage:currentPage+1];
+    }else if(currentPage+1 == bannerCount){
+        CGPoint point = CGPointMake(0,0);
+        [_bannerScrollView setContentOffset:point animated:YES];
+        [_pageControl setCurrentPage:0];
+    }
 }
 -(void)initTeacherInfo{
     _teacherInfo = [self.passedParams objectForKey:@"teacherInfo"];
