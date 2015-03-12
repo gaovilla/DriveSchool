4972249cd3b757bb08f86316e2822386dde77fec
gaovilla <apple@villa.local>
2015-03-11 21:56:27 -0700

    测试
    测试

--- DriveSchool/ViewController/DriveSchool/DSNewSubjectOneViewController.m
+++ DriveSchool/ViewController/DriveSchool/DSNewSubjectOneViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view.
     self.edgesForExtendedLayout = UIRectEdgeNone;
     self.navigationController.navigationBar.translucent = NO;
+    NSLog(@"ddddd");
 }
 
 - (void)didReceiveMemoryWarning {
